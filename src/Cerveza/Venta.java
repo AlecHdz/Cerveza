508104caa1581fe4ba8dbd98ad262c0925545014
danielromeror32 <daniel321789@gmail.com>
2021-09-20 09:47:34 -0500

    version 1.1

---
+++ src/Cerveza/Venta.java
@@ -0,0 +1,77 @@
+
+package Cerveza;
+
+
+public class Venta {
+    
+    private int idventa;
+    private int cantidad;
+    private String fecha;
+    private int preciouni;
+    private int idexpendio;
+    private int idcerveza;
+
+    public void setVenta (String _idventa, String _cantidad, String _fecha, String _preciouni,
+            String _idexpendio, String _idcerveza){
+        
+        setIdventa(Integer.parseInt(_idventa));
+        setCantidad(Integer.parseInt(_cantidad));
+        setFecha(_fecha);
+        setPreciouni(Integer.parseInt(_preciouni));
+        setIdexpendio(Integer.parseInt(_idexpendio));
+        setIdcerveza(Integer.parseInt(_idcerveza));  
+    }
+    
+    
+    
+    public int getIdventa() {
+        return idventa;
+    }
+
+    public void setIdventa(int idventa) {
+        this.idventa = idventa;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getPreciouni() {
+        return preciouni;
+    }
+
+    public void setPreciouni(int preciouni) {
+        this.preciouni = preciouni;
+    }
+
+    public int getIdexpendio() {
+        return idexpendio;
+    }
+
+    public void setIdexpendio(int idexpendio) {
+        this.idexpendio = idexpendio;
+    }
+
+    public int getIdcerveza() {
+        return idcerveza;
+    }
+
+    public void setIdcerveza(int idcerveza) {
+        this.idcerveza = idcerveza;
+    }
+    
+    
+    
+}
